4ec8afb39bd90acfd8ac130105fc77a230fb29bc
Trung HT <tieupink312@gmail.com>
2024-11-25 14:23:29 +0700

    test

--- telegram_notify.sh
+++ telegram_notify.sh
@@ -3,7 +3,8 @@
 # Token và Chat ID
 BOT_TOKEN="7940588459:AAHMJG21fOgfd9h8QcpZnNg2_thd33MFFc8" 
 CHAT_ID="-4531872882"     
-MESSAGE="$1"                
+MESSAGE="$1"    
+            
 
 # Gửi thông báo qua Telegram
 curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
