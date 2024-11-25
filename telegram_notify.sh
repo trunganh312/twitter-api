9ac7e2360acede0e9b16b1b6c4754aa856923c39
Trung HT <tieupink312@gmail.com>
2024-11-25 14:32:05 +0700

    Nữa nè

--- telegram_notify.sh
+++ telegram_notify.sh
@@ -5,8 +5,7 @@ BOT_TOKEN="7940588459:AAHMJG21fOgfd9h8QcpZnNg2_thd33MFFc8"
 CHAT_ID="-4531872882"     
 MESSAGE="$1"    
 
-
-# Gửi thông báo qua Telegram
+# Đảm bảo rằng nội dung thông báo được mã hóa đúng UTF-8
 ENCODED_MESSAGE=$(echo "$MESSAGE" | iconv -f utf-8 -t utf-8//IGNORE)
 
 # Gửi thông báo qua Telegram với mã hóa UTF-8
