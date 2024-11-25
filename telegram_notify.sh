b149e80cbc47cc7f6aa44810dc70585612d5bc09
Trung HT <tieupink312@gmail.com>
2024-11-25 14:16:58 +0700

    commit

--- telegram_notify.sh
+++ telegram_notify.sh
@@ -7,5 +7,6 @@ MESSAGE="$1"
 
 # Gửi thông báo qua Telegram
 curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
--d chat_id="${CHAT_ID}" \
--d text="${MESSAGE}"
+  -d chat_id="${CHAT_ID}" \
+  -d parse_mode="Markdown" \
+  -d text="${MESSAGE}"
