52f686a5c049826d898ca6a4bf91708a5c1b7baa
Trung HT <tieupink312@gmail.com>
2024-11-25 14:13:42 +0700

    test commit

---
+++ telegram_notify.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Token và Chat ID
+BOT_TOKEN="7940588459:AAHMJG21fOgfd9h8QcpZnNg2_thd33MFFc8" 
+CHAT_ID="-4531872882"     
+MESSAGE="$1"                
+
+# Gửi thông báo qua Telegram
+curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
+-d chat_id="${CHAT_ID}" \
+-d text="${MESSAGE}"
