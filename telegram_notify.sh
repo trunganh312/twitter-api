3333b83d6f77e72151167e7972720698d8d945d9
Trung HT <tieupink312@gmail.com>
2024-11-25 14:48:14 +0700

    test commit nè

--- telegram_notify.sh
+++ telegram_notify.sh
@@ -3,14 +3,10 @@
 # Token và Chat ID
 BOT_TOKEN="7940588459:AAHMJG21fOgfd9h8QcpZnNg2_thd33MFFc8" 
 CHAT_ID="-4531872882"     
-MESSAGE="$1"    
+MESSAGE="$1"                
 
-# Đảm bảo rằng nội dung thông báo được mã hóa đúng UTF-8
-ENCODED_MESSAGE=$(echo "$MESSAGE" | iconv -f utf-8 -t utf-8//IGNORE)
-
-
-# Gửi thông báo qua Telegram với mã hóa UTF-8
+# Gửi thông báo qua Telegram
 curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
-  --data-urlencode "chat_id=${CHAT_ID}" \
-  --data-urlencode "parse_mode=Markdown" \
-  --data-urlencode "text=${ENCODED_MESSAGE}"
+  -d chat_id="${CHAT_ID}" \
+  -d parse_mode="Markdown" \
+  -d text="${MESSAGE}"
