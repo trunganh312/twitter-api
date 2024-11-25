b546bfa8fe58732db7789c59250d66cc5b09654f
Trung HT <tieupink312@gmail.com>
2024-11-25 14:30:49 +0700

    test

--- telegram_notify.sh
+++ telegram_notify.sh
@@ -7,7 +7,10 @@ MESSAGE="$1"
 
 
 # Gửi thông báo qua Telegram
+ENCODED_MESSAGE=$(echo "$MESSAGE" | iconv -f utf-8 -t utf-8//IGNORE)
+
+# Gửi thông báo qua Telegram với mã hóa UTF-8
 curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
   --data-urlencode "chat_id=${CHAT_ID}" \
   --data-urlencode "parse_mode=Markdown" \
-  --data-urlencode "text=${MESSAGE}"
+  --data-urlencode "text=${ENCODED_MESSAGE}"
