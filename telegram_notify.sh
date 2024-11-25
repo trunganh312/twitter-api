6c455209e1a889588b73056db01575bd2fb3bdd6
Trung HT <tieupink312@gmail.com>
2024-11-25 14:25:16 +0700

    test

--- telegram_notify.sh
+++ telegram_notify.sh
@@ -4,10 +4,10 @@
 BOT_TOKEN="7940588459:AAHMJG21fOgfd9h8QcpZnNg2_thd33MFFc8" 
 CHAT_ID="-4531872882"     
 MESSAGE="$1"    
-            
+
 
 # Gửi thông báo qua Telegram
 curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
-  -d chat_id="${CHAT_ID}" \
-  -d parse_mode="Markdown" \
-  -d text="${MESSAGE}"
+  --data-urlencode "chat_id=${CHAT_ID}" \
+  --data-urlencode "parse_mode=Markdown" \
+  --data-urlencode "text=${MESSAGE}"
